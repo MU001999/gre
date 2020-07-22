39c074ba8ef02b60afc41e888444639ff8213d34
mu001999 <mu001999@outlook.com>
2020-07-23 00:16:30 +0800

    update

--- src/gre.hpp
+++ src/gre.hpp
@@ -9,10 +9,26 @@ namespace details
 
 } // namespace details
 
+class Options
+{
+  public:
+    Options() = default;
+  private:
+};
+
 class GRE
 {
   public:
     GRE(const std::string &pattern);
+    GRE(const std::string &pattern, const Options &Options);
+
+    const std::string &pattern() const;
+
+    template<typename ...Args>
+    static bool
+    full_match(const std::string &text,
+        const GRE &re, Args &...args);
+
   private:
 };
 } // namespace gre;

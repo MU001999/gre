e8e47ffe2039cc214aa5a1763c97f3c3f5c1ed71
mu001999 <mu001999@outlook.com>
2020-09-04 16:45:48 +0800

    update test

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,61 @@ using namespace gre;
 
 TEST(Simple, SingleChar)
 {
-    ASSERT_EQ(GRE::full_match(" "s, " ").value(), " ");
+    ASSERT_EQ(GRE::full_match("a"s, "a").value().self(), "a");
+}
+
+TEST(Capture, Multi)
+{
+    auto re = GRE("(a)*");
+    auto result = re.match("aaaaa").value();
+
+    ASSERT_EQ(result.self(), "aaaaa");
+    ASSERT_EQ(result.subs().size(), 5);
+
+    for (const auto &sub : result[0])
+    {
+        ASSERT_EQ(sub, "a");
+    }
+}
+
+TEST(Capture, Named)
+{
+    auto re = GRE("(?<test>a)*");
+    auto result = re.match("aaaaa").value();
+
+    ASSERT_EQ(result.self(), "aaaaa");
+    ASSERT_EQ(result.subs().size(), 5);
+
+    for (const auto &sub : result["test"])
+    {
+        ASSERT_EQ(sub, "a");
+    }
+}
+
+TEST(Capture, SimpleNested)
+{
+    auto re = GRE("(?<1>(?<2>a)*(?<3>b))*");
+    auto result = re.match("babaabaaab").value();
+
+    ASSERT_EQ(result.self(), "babaabaaab");
+    ASSERT_EQ(result.subs().size(), 4);
+
+    int i = 0;
+    for (auto &sub : result.subs("1"))
+    {
+        ASSERT_EQ(sub.get().self(), string(i, 'a') + "b");
+
+        auto subs2 = sub.get()["2"];
+        ASSERT_EQ(subs2.size(), i++);
+        for (const auto &str : subs2)
+        {
+            ASSERT_EQ(str, "a");
+        }
+
+        auto subs3 = sub.get()["3"];
+        ASSERT_EQ(subs3.size(), 1);
+        ASSERT_EQ(subs3[0], "b");
+    }
 }
 
 int main(int argc, char *argv[])

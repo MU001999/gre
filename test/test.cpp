0e974519bebbe721cca26fae11f0415b93c37e1a
mu001999 <mu001999@outlook.com>
2020-09-06 11:17:41 +0800

    update test to fit new gre

--- test/test.cpp
+++ test/test.cpp
@@ -7,12 +7,12 @@ using namespace gre;
 
 TEST(SingleChar, Alpha)
 {
-    ASSERT_EQ(GRE::full_match("a"s, "a").value().self(), "a");
+    ASSERT_EQ(GRE::full_match("a"s, "a").value(), "a"s);
 }
 
 TEST(SingleChar, Blank)
 {
-    ASSERT_EQ(GRE::full_match(" "s, "  ").value().self(), " ");
+    ASSERT_EQ(GRE::full_match(" "s, "  ").value(), " "s);
 }
 
 TEST(Capture, Multi)
@@ -20,12 +20,12 @@ TEST(Capture, Multi)
     auto re = GRE("(a)*");
     auto result = re.match("aaaaa").value();
 
-    ASSERT_EQ(result.self(), "aaaaa");
+    ASSERT_EQ(result, "aaaaa"s);
     ASSERT_EQ(result.subs().size(), 5);
 
     for (const auto &sub : result[0])
     {
-        ASSERT_EQ(sub, "a");
+        ASSERT_EQ(sub, "a"s);
     }
 }
 
@@ -34,12 +34,12 @@ TEST(Capture, Named)
     auto re = GRE("(?<test>a)*");
     auto result = re.match("aaaaa").value();
 
-    ASSERT_EQ(result.self(), "aaaaa");
+    ASSERT_EQ(result, "aaaaa"s);
     ASSERT_EQ(result.subs().size(), 5);
 
     for (const auto &sub : result["test"])
     {
-        ASSERT_EQ(sub, "a");
+        ASSERT_EQ(sub, "a"s);
     }
 }
 
@@ -48,24 +48,24 @@ TEST(Capture, SimpleNested)
     auto re = GRE("(?<1>(?<2>a)*(?<3>b))*");
     auto result = re.match("babaabaaab").value();
 
-    ASSERT_EQ(result.self(), "babaabaaab");
+    ASSERT_EQ(result, "babaabaaab"s);
     ASSERT_EQ(result.subs().size(), 4);
 
     int i = 0;
-    for (auto &sub : result.subs("1"))
+    for (auto &sub : result["1"])
     {
-        ASSERT_EQ(sub.get().self(), string(i, 'a') + "b");
+        ASSERT_EQ(sub, string(i, 'a') + "b");
 
-        auto subs2 = sub.get()["2"];
+        auto subs2 = sub["2"];
         ASSERT_EQ(subs2.size(), i++);
         for (const auto &str : subs2)
         {
-            ASSERT_EQ(str, "a");
+            ASSERT_EQ(str, "a"s);
         }
 
-        auto subs3 = sub.get()["3"];
+        auto subs3 = sub["3"];
         ASSERT_EQ(subs3.size(), 1);
-        ASSERT_EQ(subs3[0], "b");
+        ASSERT_EQ(subs3[0], "b"s);
     }
 }
 
